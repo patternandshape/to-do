84fdebb9a2056faf0ee8d58d9de635a36f26083c
Molly Curtin and Sean Peerenboom <patternandshape@gmail.com>
2016-01-25 14:15:35 -0800

    add basic business script and form on html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,26 @@
-function cityCountry(cityName, countryName) {
-  this.cityName = cityName;
-  this.countryName = countryName;
+function item(itemName) {
+  this.itemName = itemName;
 
-}
-
-cityCountry.prototype.fullPlace = function() {
-  return this.cityName + ", " + this.countryName;
-}
-
-function cityInfo(city, country, landmark, timeOfYear, notes) {
-  this.city = city;
-  this.country = country;
-  this.landmark = landmark;
-  this.timeOfYear = timeOfYear;
-  this.notes = notes;
 }
 
 $(document).ready(function() {
-  $("form#new-place").submit(function(event) {
+  $("form#new-task").submit(function(event) {
     event.preventDefault();
-    var pCity = $("input#city").val();
-    var pCountry = $("input#country").val();
-    var newPlace = new cityCountry(pCity, pCountry);
-    var pLandmark = $("input#landmark").val();
-    var pTimeOfYear = $("input#timeOfYear").val();
-    var pNotes = $("input#notes").val();
-    var newCityInfo = new cityInfo(pCity, pCountry, pLandmark, pTimeOfYear, pNotes);
+    var userTask = $("input#item").val();
+    // var pCountry = $("input#country").val();
+    // var newPlace = new cityCountry(pCity, pCountry);
+    // var pLandmark = $("input#landmark").val();
+    // var pTimeOfYear = $("input#timeOfYear").val();
+    // var pNotes = $("input#notes").val();
+    // var newCityInfo = new cityInfo(pCity, pCountry, pLandmark, pTimeOfYear, pNotes);
 
-    $("ul#places").append("<li><span class='place'>" + newPlace.fullPlace() + "</span></li>");
-    $("input#city").val("");
-    $("input#country").val("");
+    $("ul#userList").append("<li><span class='task'>" + userTask + "</span></li>");
+    $("input#item").val();
 
-  $(".place").last().click(function() {
+  $(".task").last().click(function() {
   $("#show-place").show();
   $("#show-place h2").text(newPlace.fullPlace());
   $(".city-name").text(newCityInfo.city);
-  $(".country-name").text(newCityInfo.country);
-  $(".landmark-name").text(newCityInfo.landmark);
-  $(".best-time").text(newCityInfo.timeOfYear);
-  $(".notes").text(newCityInfo.notes);
 });
 });
 });
