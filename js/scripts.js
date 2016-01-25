8ff61c5808893da1e4bd2c3aa7fd3f5ba4e0c962
Molly Curtin and Sean Peerenboom <patternandshape@gmail.com>
2016-01-25 15:16:41 -0800

    add final html and working draft of strike-through feature

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,24 @@
 function item(itemName) {
   this.itemName = itemName;
-
 }
 
 $(document).ready(function() {
   $("form#new-task").submit(function(event) {
     event.preventDefault();
     var userTask = $("input#item").val();
-    // var pCountry = $("input#country").val();
-    // var newPlace = new cityCountry(pCity, pCountry);
-    // var pLandmark = $("input#landmark").val();
-    // var pTimeOfYear = $("input#timeOfYear").val();
-    // var pNotes = $("input#notes").val();
-    // var newCityInfo = new cityInfo(pCity, pCountry, pLandmark, pTimeOfYear, pNotes);
 
     $("ul#userList").append("<li><span class='task'>" + userTask + "</span></li>");
     $("input#item").val();
 
+$(".task").click(function () {
+$(".task").wrap("<strike>");
+$(".task").fadeOut("slow");
+});
+
   $(".task").last().click(function() {
   $("#show-place").show();
-  $("#show-place h2").text(newPlace.fullPlace());
-  $(".city-name").text(newCityInfo.city);
+  // $("#show-place h2").text(newPlace.fullPlace());
+  $("ul#compList").append("<li><span class='task'>" + userTask + "</span></li>");
 });
 });
 });
